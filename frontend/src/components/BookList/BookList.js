ee52130ca490bc7b18199b44b95b84a60e3e7316
wintermute28 <a28c@yandex.ru>
2024-01-25 13:13:49 +0300

    Added BookForm, BookList and Filter components

---
+++ frontend/src/components/BookList/BookList.js
@@ -0,0 +1,11 @@
+import './BookList.css';
+
+const BookList = () => {
+  return (
+    <div className="app-block book-list">
+        <h2>BookList</h2>
+    </div>
+  )
+}
+
+export default BookList;
