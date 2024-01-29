e6655aefb1083e436fc7d7325a05659062f02095
wintermute28 <a28c@yandex.ru>
2024-01-29 12:30:44 +0300

    Added book ID

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../../redux/books/actionCreators';
 import './BookForm.css';
 
@@ -13,8 +14,9 @@ const BookForm = () => {
     
     if(title && author) {
       const book = {
-        title: title,
-        author: author
+        title,
+        author,
+        id: uuidv4()
       };
       console.log(addBook(book));
       dispatch(addBook(book));
