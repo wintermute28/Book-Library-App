30ffbc2d7f2cfa38e3971db4d40522471743bc92
wintermute28 <a28c@yandex.ru>
2024-01-31 18:22:33 +0300

    Added filter by author

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,14 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { setTitleFilter, resetFilters, selectTitleFilter } from '../../redux/slices/filterSlice';
+import { setTitleFilter,
+         resetFilters,
+         setAuthorFilter,
+         selectTitleFilter,
+         selectAuthorFilter } from '../../redux/slices/filterSlice';
 import './Filter.css';
 
 const Filter = () => {
   const dispatch = useDispatch();
   const titleFilter = useSelector(selectTitleFilter);
+  const authorFilter = useSelector(selectAuthorFilter);
 
   const handleTitleFilterChange = (e) => {
     dispatch(setTitleFilter(e.target.value));
   };
+
+  const handAuthorFilterChange = (e) => {
+    dispatch(setAuthorFilter(e.target.value));
+  }
 
   const handleResetFilters = () => {
     dispatch(resetFilters());
@@ -23,6 +32,13 @@ const Filter = () => {
             value={titleFilter}
             onChange={handleTitleFilterChange}
             placeholder='Filter by title...' />
+        </div>
+        <div className="filter-group">
+          <input
+            type="text"
+            value={authorFilter}
+            onChange={handAuthorFilterChange}
+            placeholder='Filter by author...' />
         </div>
         <button
           type="button"
--- frontend/src/redux/slices/filterSlice.js
+++ frontend/src/redux/slices/filterSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    title: ''
+    title: '',
+    author: ''
 };
 
 const filterSlice = createSlice({
@@ -15,14 +16,18 @@ const filterSlice = createSlice({
             //You can also return new state as usually
             // return {...state, title: action.payload};
         },
+        setAuthorFilter: (state, action) => {
+            state.author = action.payload;
+        },
         resetFilters: (state) => {
             return initialState
-        }
+        },
     }
 });
 
-export const { setTitleFilter, resetFilters } = filterSlice.actions;
+export const { setTitleFilter,setAuthorFilter, resetFilters  } = filterSlice.actions;
 
 export const selectTitleFilter = (state) => state.filter.title;
+export const selectAuthorFilter = (state) => state.filter.author;
 
 export default filterSlice.reducer;
